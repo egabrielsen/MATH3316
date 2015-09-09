f660e37ee02edd6d24ff7ea5268f774628d0c53a
egabrielsen <erik.gabe@gmail.com>
2015-09-08 21:31:41 -0500

    printing wrong numbers but screw it

--- Project1/nest.cpp
+++ Project1/nest.cpp
@@ -3,7 +3,11 @@
 using namespace std;
 
 double nest(Matrix &a, double x) {
-  // a is the coefficient matrix, x is the value passed in, 
-  double p = 10;
+  // a is the coefficient matrix, x is the value passed in,
+  int size = a.Size();
+  double p = a(size - 1);
+  for (int i = (size - 2) ; i >= 0; i--) {
+    p = a(i) + (x * p);
+  }
   return p;
 }
--- Project1/proj1_a.cpp
+++ Project1/proj1_a.cpp
@@ -4,7 +4,7 @@
 #include "nest.cpp"
 
 using namespace std;
-void computeF(Matrix &a);
+void computeF(Matrix &z);
 double factorial(double x, double result);
 
 int main() {
@@ -12,36 +12,45 @@ int main() {
 	Matrix z = Linspace(-3.0, 3.0, 601, 1);
 	z.Write("z.txt");
 
-	// -- Coefficients 
-	double p4data[] = {factorial(1, 1), factorial(2, 1), factorial(4, 1)};
-	double p8data[] = {factorial(1, 1), factorial(2, 1), factorial(4, 1), factorial(6, 1), factorial(8, 1)};
-	double p12data[] = {factorial(1, 1), factorial(2, 1), factorial(4, 1), factorial(6, 1),factorial(8, 1), factorial(10, 1), factorial(12, 1)};
+	// -- Coefficients
+	double p4data[] = {1, (-1/factorial(2, 1)), (1/factorial(4, 1))};
+	double p8data[] = {(1/factorial(1, 1)), (-1/factorial(2, 1)), (1/factorial(4, 1)), (-1/factorial(6, 1)), (1/factorial(8, 1))};
+	double p12data[] = {(1/factorial(1, 1)), (-1/factorial(2, 1)), (1/factorial(4, 1)), (-1/factorial(6, 1)), (1/factorial(8, 1)), (-1/factorial(10, 1)), (1/factorial(12, 1))};
 
 	// -- Coefficient matrices for each polynomial
-	Matrix p4coeff(1, 3, p4data);
-	Matrix p8coeff(1, 5, p8data);
-	Matrix p12coeff(1, 7, p12data);
+	Matrix p4coeff(3, 1, p4data);
+	Matrix p8coeff(5, 1, p8data);
+	Matrix p12coeff(7, 1, p12data);
 
 	// -- Matrix for polynomials, initialized to 0
-	Matrix p4(1, 600);
-	Matrix p8(1, 600);
-	Matrix p12(1, 600);
-	cout << p4 << endl;
-	cout << p8 << endl;
-	cout << p12 << endl;
+	Matrix p4(601, 1);
+	Matrix p8(601, 1);
+	Matrix p12(601, 1);
 
-	// cout << nest(p[0], 4);
-	// for(int i = 0; i <= 600; i++) {
-	// 	p4[0][i] = nest(a, 4)
-	// }
-	// computeF(z);
+	// -- Creating p4, p8, and p12 matrices
+	for(int i = 0; i < z.Size(); i++) {
+		p4(i) = nest(p4coeff, z(i));
+	}
+	p4.Write("p4.txt");
+
+	for(int i = 0; i < z.Size(); i++) {
+		p8(i) = nest(p8coeff, z(i));
+	}
+	p8.Write("p8.txt");
+
+	for(int i = 0; i < z.Size(); i++) {
+		p12(i) = nest(p12coeff, z(i));
+	}
+	p12.Write("p12.txt");
+	computeF(z);
+
 	return 0;
 }
 
-void computeF(Matrix &a) {
-	Matrix f = a;
-	for(int i = 0; i <= 600; i++) {
-		f[0][i] = cos(f[0][i]);
+void computeF(Matrix &z) {
+	Matrix f(601, 1);
+	for(int i = 0; i < f.Size(); i++) {
+		f(i) = cos(z(i));
 	}
 	f.Write("f.txt");
 }
