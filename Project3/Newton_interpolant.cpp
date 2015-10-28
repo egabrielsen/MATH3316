85ef54a06426a03a6022f019a554e7e522f805a8
egabrielsen <erik.gabe@gmail.com>
2015-10-28 02:08:58 -0500

    WIP

--- Project3/Newton_interpolant.cpp
+++ Project3/Newton_interpolant.cpp
@@ -1,15 +1,41 @@
 /* Erik Gabrielsen
    MATH 3316
-   Program 3 */
+   Program 3
+   October 26th 2015*/
 
 #include <iostream>
 #include <stdlib.h>
 #include <stdio.h>
 #include <iostream>
 #include <math.h>
-#include "martix.hpp"
+#include "matrix.hpp"
 
 using namespace std;
 
-Matrix Newton_coefficients(Matrix& x, Matrix& y);
-double Newton_evaluate(Matrix& x, Matrix& c, double z);
+Matrix Newton_coefficients(Matrix& x, Matrix& y) {
+  int n = y.Size() - 1;
+  Matrix a(n+1);
+
+  for (int i = 0; i < n; i++) {
+    a(i) = y(i);
+  }
+
+
+
+  for (int j = 1; j < n; j++) {
+    for (int i = n; i > j; i-- ) {
+      a(i) = (a(i) - a(i-1))/(x(i) - x(i-j));
+    }
+  }
+  return a;
+}
+
+double Newton_evaluate(Matrix& x, Matrix& c, double z) {
+  int n = c.Size() - 1;
+  double temp = c(n);
+  for (int i = n-1; i > 0; i--) {
+    temp *= (z - x(i));
+    temp += c(i);
+  }
+  return temp;
+}
--- Project3/test_Newton.cpp
+++ Project3/test_Newton.cpp
@@ -1,3 +1,55 @@
 /* Erik Gabrielsen
    MATH 3316
    Program 3 */
+
+   // Inclusions
+   #include <stdlib.h>
+   #include <stdio.h>
+   #include <iostream>
+   #include <math.h>
+   #include "matrix.hpp"
+
+   using namespace std;
+
+
+   // function prototypes
+   Matrix Newton_coefficients(Matrix& x, Matrix& y);
+   double Newton_evaluate(Matrix& x, Matrix& c, double z);
+   double f(double x) {
+     return cosh(2.0*x*x);
+   }
+
+
+   // This routine tests the function lagrange.cpp
+  int main(int argc, char* argv[]) {
+
+    vector<size_t> nvals = {10, 20};
+    for (size_t k=0; k<nvals.size(); k++) {
+      int n = nvals[k];
+      cout << endl << "interpolants and errors using " << n+1 << " nodes:\n";
+
+      Matrix x = Linspace(0.0, 1.0, n+1, 1);
+      Matrix y(n+1);
+      for(int i = 0; i <= n; i++) {
+          y(i) = f(x(i));
+      }
+      double dx = 1.0/n;
+      Matrix z = Linspace(dx/2.0, 1.0-dx/2.0, n, 1); // evaluation points
+
+      Matrix p(n); // results of interpolation
+      Matrix c(n+1); // coefficients
+      c = Newton_coefficients(x, y); // only need to calculate the coefficients once
+      for(int i = 0; i < n; i++) {
+          // calculate Newton interpolant at each evaluation point
+          p(i) = Newton_evaluate(x, c, z(i));
+      }
+
+      std::cout << "interpolant/error w/ " << n+1 << " nodes: " << std::endl;
+      std::cout << "    z        f(z)               p(z)              error" << std::endl;
+      for(int i = 0; i < n; i++) {
+          printf("   %6.3f   %16.13f   %16.13f   %7.2g\n", z(i), f(z(i)), p(i), fabs(f(z(i)) - p(i)));
+      }
+
+    }
+    return 0;
+  }
