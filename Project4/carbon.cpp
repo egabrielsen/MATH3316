4542fb8c36c9e5de9fb5e9da8a4d7a8da5efc8ae
egabrielsen <erik.gabe@gmail.com>
2015-11-27 18:24:05 -0600

    everything is set up for Project 4 part 3

---
+++ Project4/carbon.cpp
@@ -0,0 +1,22 @@
+/* Erik Gabrielsen
+  Math 3316
+  27 November 2015 */
+
+#include <iostream>
+#include <cmath>
+#include "fcn.hpp"
+
+using namespace std;
+
+// including adaptive_int
+int adaptive_int(Fcn& f, const double a, const double b, const double rtol, const double atol, double& R, int& n, int& Ntot);
+
+double erf(const double y, const double rtol, const double atol) {
+  double R;
+  int n, nTol;
+  return 0;
+}
+
+double carbon(const double x, const double t, const double T, const double rtol, const double atol) {
+  return 0;
+}
---
+++ Project4/test_carbon.cpp
@@ -0,0 +1,18 @@
+/* Erik Gabrielsen
+  Math 3316
+  27 November 2015 */
+
+#include <iostream>
+#include <cmath>
+#include "fcn.hpp"
+
+using namespace std;
+
+double erf(const double y, const double rtol, const double atol);
+double carbon(const double x, const double t, const double T, const double rtol, const double atol);
+
+int main() {
+
+
+  return 0;
+}
