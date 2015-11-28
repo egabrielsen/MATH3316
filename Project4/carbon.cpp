5fc9277725632def1c421da534d93161f52c2b9c
egabrielsen <erik.gabe@gmail.com>
2015-11-27 21:37:05 -0600

    c++ project 3 might be done

--- Project4/carbon.cpp
+++ Project4/carbon.cpp
@@ -8,15 +8,37 @@
 
 using namespace std;
 
+const double PI = atan(1.0)*4;
+
 // including adaptive_int
 int adaptive_int(Fcn& f, const double a, const double b, const double rtol, const double atol, double& R, int& n, int& Ntot);
 
+class fcn : public Fcn {
+public:
+  double operator()(double z) {   // function evaluation of e^((-z)^2)
+    return exp(-(z*z));
+  }
+};
+
+// -- initial values of C0
+const double C0 = .001;
+const double Cs = .02;
+
 double erf(const double y, const double rtol, const double atol) {
+  fcn f; // can I instead pass this as an argument to erf?
   double R;
-  int n, nTol;
-  return 0;
+  int n, nTot;
+
+  if (adaptive_int(f, 0, y, rtol, atol, R, n, nTot) == 1) {
+    R *= 2.0/sqrt(PI);
+  }
+  return R;
+}
+
+double D(const double T) {
+  return 6.2e-7*exp(-8e4/(8.31*T));
 }
 
 double carbon(const double x, const double t, const double T, const double rtol, const double atol) {
-  return 0;
+  return Cs - (Cs-C0)* erf((x/sqrt(4*t*D(T))), rtol, atol);
 }
--- Project4/test_carbon.cpp
+++ Project4/test_carbon.cpp
@@ -5,14 +5,63 @@
 #include <iostream>
 #include <cmath>
 #include "fcn.hpp"
+#include "matrix.hpp"
 
 using namespace std;
 
-double erf(const double y, const double rtol, const double atol);
 double carbon(const double x, const double t, const double T, const double rtol, const double atol);
 
+const double atolerance = 1e-11;
+const double rtolerance = 1e-15;
+
 int main() {
+  Matrix K = Linspace(800, 1200, 401); // Kelvin
+  Matrix t = Linspace(1, 172800, 601); // temp to keep track of time 1s - 48 hours (172800 s)
+  Matrix c2mm = Matrix(400, 600);
+  Matrix c4mm = Matrix(400, 600);
+  Matrix C2mm_800K = Matrix(600);
+  Matrix C2mm_900K = Matrix(600);
+  Matrix C2mm_1000K = Matrix(600);
+  Matrix C2mm_1100K = Matrix(600);
+  Matrix C2mm_1200K = Matrix(600);
+  Matrix C4mm_800K = Matrix(600);
+  Matrix C4mm_900K = Matrix(600);
+  Matrix C4mm_1000K = Matrix(600);
+  Matrix C4mm_1100K = Matrix(600);
+  Matrix C4mm_1200K = Matrix(600);
 
+  for (int i = 0; i < 400; i++) {
+    for (int j = 0; j< 600; j++) {
+      c2mm(i, j) = carbon(.002, t(j), K(i), rtolerance, atolerance);
+      c4mm(i, j) = carbon(.004, t(j), K(i), rtolerance, atolerance);
+    }
+  }
+  for (int i = 0; i < 600; i++) {
+    C2mm_800K(i) = carbon(.002, t(i), 800, rtolerance, atolerance);
+    C2mm_900K(i) = carbon(.002, t(i), 900, rtolerance, atolerance);
+    C2mm_1000K(i) = carbon(.002, t(i), 1000, rtolerance, atolerance);
+    C2mm_1100K(i) = carbon(.002, t(i), 1100, rtolerance, atolerance);
+    C2mm_1200K(i) = carbon(.002, t(i), 1200, rtolerance, atolerance);
+    C4mm_800K(i) = carbon(.004, t(i), 800, rtolerance, atolerance);
+    C4mm_900K(i) = carbon(.004, t(i), 900, rtolerance, atolerance);
+    C4mm_1000K(i) = carbon(.004, t(i), 1000, rtolerance, atolerance);
+    C4mm_1100K(i) = carbon(.004, t(i), 1100, rtolerance, atolerance);
+    C4mm_1200K(i) = carbon(.004, t(i), 1200, rtolerance, atolerance);
+  }
 
+  K.Write("Temp.txt");
+  t.Write("time.txt");
+  c2mm.Write("c2mm.txt");
+  c4mm.Write("c4mm.txt");
+  C2mm_800K.Write("C2mm_800K.txt");
+  C2mm_900K.Write("C2mm_900K.txt");
+  C2mm_1000K.Write("C2mm_1000K.txt");
+  C2mm_1100K.Write("C2mm_1100K.txt");
+  C2mm_1200K.Write("C2mm_1200K.txt");
+  C4mm_800K.Write("C4mm_800K.txt");
+  C4mm_900K.Write("C4mm_900K.txt");
+  C4mm_1000K.Write("C4mm_1000K.txt");
+  C4mm_1100K.Write("C4mm_1100K.txt");
+  C4mm_1200K.Write("C4mm_1200K.txt");
   return 0;
 }
